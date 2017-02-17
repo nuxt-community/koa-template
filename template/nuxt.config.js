bb8d5583ae09f7cc139662be0f12f65d3eeb1540
Sébastien Chopin <seb@chopin.io>
2017-02-17 22:17:43 +0100

    Update nuxt.config.js

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -7,10 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
