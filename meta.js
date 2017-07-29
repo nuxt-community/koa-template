eac09a341f8e4295b48c7f4ac874f078d59668a0
Ilya Semenov <semenov@inetss.com>
2017-07-29 22:02:53 +0700

    Use nuxt@1.0.0-rc3. Drop support for Koa 1.x

--- meta.js
+++ meta.js
@@ -3,11 +3,7 @@ module.exports = {
     raw: function(options) {
       return options.fn(this)
     },
-    koaSemver: function() {
-      return this.koaVersion === '1.x' ? '^1.2.5' : '^2.0.0'
-    },
     ifBabel: function(options) {
-      if (this.koaVersion !== '2.x') return ''
       var nodeMajMin = process.version.match(/v(\d*).(\d*)./)
       var semMaj = Number(nodeMajMin[1])
       var semMin = Number(nodeMajMin[2])
@@ -30,13 +26,6 @@ module.exports = {
     author: {
       'type': 'string',
       'message': 'Author'
-    },
-    koaVersion: {
-      'type': 'list',
-      'required': true,
-      'message': 'Koa version',
-      'choices': ['1.x', '2.x'],
-      'default': 0
     }
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
--- template/server/index.js
+++ template/server/index.js
@@ -1,34 +1,45 @@
 import Koa from 'koa'
-import Nuxt from 'nuxt'
+import { Nuxt, Builder } from 'nuxt'
 
 const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
-// Start nuxt.js
-async function start () {
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
-  // Instanciate nuxt.js
-  const nuxt = await new Nuxt(config)
-  // Build in development
-  if (config.dev) {
-    try {
-      await nuxt.build()
-    } catch (e) {
-      console.error(e) // eslint-disable-line no-console
-      process.exit(1)
-    }
-  }
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(app.env === 'production')
 
-  app.use(async (ctx, next) => {
-  ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    await nuxt.render(ctx.req, ctx.res)
+// Instantiate nuxt.js
+const nuxt = new Nuxt(config)
+
+// Build in development
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build().catch(e => {
+    console.error(e) // eslint-disable-line no-console
+    process.exit(1)
   })
-
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
 }
 
-start()
+app.use(async ctx => {
+  ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+
+  return new Promise((resolve, reject) => {
+    const mockedResponse = {
+      __proto__: ctx.res,
+      end() {
+        ctx.res.end.apply(ctx.res, arguments)
+        // nuxt.render doesn't call next() on successful render,
+        // resolve promise manually
+        resolve()
+      }
+    }
+    nuxt.render(ctx.req, mockedResponse, promise => {
+      // nuxt.render passes a rejected promise into callback on error.
+      promise.then(resolve).catch(reject)
+    })
+  })
+})
+
+app.listen(port, host)
+console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
