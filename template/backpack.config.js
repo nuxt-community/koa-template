6c8a35e71f82813db63c0a0e5a361d88ebfc98db
you06 <you1474600@gmail.com>
2018-11-03 17:05:30 +0800

    upgrade dependencies and config, fix #49, #50, #52

--- template/backpack.config.js
+++ template/backpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   webpack: (config, options, webpack) => {
     config.entry.main = './server/index.js'
+    config.module = {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: /(node_modules)/
+        }
+      ]
+    }
     return config
   }
 }
